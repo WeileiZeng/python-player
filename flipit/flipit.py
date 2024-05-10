a02da6a9c01082cc2dd0d647c5b7cd41a501f510
weilei@Air15 <weilei.zeng@foxmail.com>
2024-05-10 16:13:04 +0800

    polish doc

--- flipit/flipit.py
+++ flipit/flipit.py
@@ -1,12 +1,16 @@
-
 #play the game for flip it
+# Author: Weilei Zeng
+# Dated: May 10, 2024
 
 from graphics import *
 from button import Button
 import random
 import time
+import numpy as np
 
-def get_beighbors(rows,cols,board):
+
+def get_neighbors(rows,cols,board):
+    #return neighbors of each plaquette. This takes care of the boundary condition
     neighbors=[]
     #placeholder=[[1 for _ in range(cols)] for _ in range(rows)]
     for i in range(rows):
@@ -36,11 +40,13 @@ def get_beighbors(rows,cols,board):
             neighbors.append(_)
     return neighbors
 
-import numpy as np
 
+#convert index of 2D array to that of a horizontally stacked vector
 def ij2index(i,j,rows,cols):
     return i*cols+j
-#there are multiple ways to get S
+
+#return parity check for graph defined by neighbors
+#there are multiple ways to get H. for example, using non-cyclic shift matrix S, H = I * I + I * S + S * I + S^T * I + I * S^T
 def get_H(neighbors,rows,cols):
     H_size = rows*cols
     H = np.zeros((H_size,H_size), dtype=int)
@@ -49,25 +55,25 @@ def get_H(neighbors,rows,cols):
         for j in range(cols):
             index_i = ij2index(i,j,rows,cols)
             neighbor = neighbors[i][j]
-            #for neighbor in neighbors:
             for n in neighbor:
                 x,y=n
                 index_j = ij2index(x,y,rows,cols)
                 H[index_i][index_j]=1
     return H
 
+#play the game on this board
 class Board():
     def __init__(self,win,rows,cols):
         self.win=win
         self.rows=rows
         self.cols=cols
-        self.board=[]
         # set a random initial board
         self.board = [[random.randint(0,1) for _ in range(self.cols)] for _ in range(self.rows)]
+        #draw buttons/plaquettes and set up color accordingly
         self.buttons=[ [None for _ in range(self.cols)] for _ in range(self.rows) ]
-        x0,y0=200,40
-        size,distance=60,10
-        self.win.autoflush=False
+        x0,y0=200,40 #top left anchor
+        size,distance=60,10 #for each plaquette
+        self.win.autoflush=False #turn it off for quick draw with many changes in the window
         for i in range(self.rows):
             for j in range(self.cols):
                 button_plaquette = Button(self.win,
@@ -75,32 +81,23 @@ class Board():
                                           width=size, height=size, label=f"({i},{j})")
                 #button_plaquette.face=1
                 button_plaquette.activate()
-                button_plaquette.face=self.board[i][j]
+                #button_plaquette.face=self.board[i][j]
                 button_plaquette.i = i
                 button_plaquette.j = j
                 #print(button_plaquette.i,button_plaquette.j)
                 if self.board[i][j]:
                     button_plaquette.rect.setFill('green')
-
                 self.buttons[i][j] = button_plaquette
-                
-        #self.win.autoflush=True
+
         self.win.flush()
 
-        self.neighbors=get_beighbors(rows,cols,self.board)   
-
+        self.neighbors=get_neighbors(rows,cols,self.board)   
         self.H=get_H(self.neighbors,self.rows,self.cols)
-        print(self.H)
-        e=np.zeros(self.rows*self.cols,int)
-        e[0]=1
-        print('error',e)
-        print(self.H.dot(e.transpose()))
 
-        self.push2right()
-        self.solve()
+        #self.push2right()
+        #self.solve()
         
     def flip(self,r,c):
-        #self.autoflush=False
         # if (r,c) gets clicked
         neighbor = self.neighbors[r][c]
         #print('the neighbor for ',r,c,'is',neighbor)
@@ -115,78 +112,67 @@ class Board():
                 self.board[i][j] = 1
                 self.buttons[i][j].rect.setFill('green')
         #self.print()
-        #self.autoflush=True
                 
-    def print(self):
+    def print(self): #print the board in std
         print('--------------------------')
         for i in self.board:
             print(i)
         print('--------------------------')
-    def check(self):
+    def check(self): #check if the whole board is cleaned. now just do it by hand
         pass
 
-    def push2right(self):
-        #self.autoflush=False
+    def push2right(self):#push all plaquettes to the rightmost column
         for j in range(self.cols-1):#check column first
             for i in range(self.rows):
                 if self.board[i][j]:
-                    self.buttons[i][j+1].rect.setFill('yellow')
+                    self.buttons[i][j+1].rect.setFill('yellow') #click the next column based on current column, this will clean the current column. Hence the loop push all colored plaquettes to the right most column
                     #self.win.flush()
                     #time.sleep(0.1)
-
                     self.flip(i,j+1)
                     self.win.flush()
                     #time.sleep(0.1)
                     
-        #self.autoflush=True
-    def solve(self):#run it after push
-        #e=np.zeros(self.rows*self.cols,int)
+    def solve(self):# solve it using linear algebra
         e_array=np.array(self.board)
+        print('current error\n',e_array)
         e = np.hstack(e_array)
-        print(self.H)
-        print('current error',e)
+        #print(self.H)
+
         s=np.linalg.solve(self.H,e)
-        print(s)
+        #print(s)
 
         import galois
         def binary_solve(A,b):
-            GF = galois.GF(2)
-            #A = GF.Random((4,4))
+            GF = galois.GF(2)            #A = GF.Random((4,4))
             A=GF(A)
-            #print(A)
             b=GF(b)
-            #x_truth = GF([1,0,1,1])    
-            #b = A @ x_truth
             x = np.linalg.solve(A, b)
-            #np.array_equal(x, x_truth)
             return x 
 
         s2=binary_solve(self.H,e)
-        print('attemplt solution',s2)
+        #print('attemplt solution',s2)
         s3=s2.reshape(self.rows,self.cols)
-        print(s3)
-        print('rank of H is ',np.linalg.matrix_rank(self.H))
-        #print in color
+        print('solution:\n',s3)
+        #print('rank of H is ',np.linalg.matrix_rank(self.H))
+        #display in color
         for i in range(self.rows):
             for j in range(self.cols):
                 b=self.buttons[i][j]
                 if s3[i][j]:
                     b.rect.setWidth(5)
                 else:
-                    b.rect.setWidth(1)
-        
+                    b.rect.setWidth(1)        
 
     def shuffle(self):#generate random board and update all buttons
-        #self.win.autoflush=False
         self.board = [[random.randint(0,1) for _ in range(self.cols)] for _ in range(self.rows)]
         for i in range(self.rows):
             for j in range(self.cols):
                 b=self.buttons[i][j]
+                b.rect.setWidth(1)
                 if self.board[i][j]:
                     b.rect.setFill('green')
                 else:
                     b.rect.setFill('lightgray')                    
-        #self.win.autoflush=True
         
 def main():
     win_width, win_height=1200,700+40
