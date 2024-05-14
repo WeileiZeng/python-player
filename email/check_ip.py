6c928e2637b27f61d7d105ff5ca2c713ef61599e
weilei@Air15 <weilei.zeng@foxmail.com>
2024-05-14 15:29:39 +0800

    reformat messages and code

--- email/check_ip.py
+++ email/check_ip.py
@@ -1,18 +1,28 @@
-# crontab -e
-# 30 * * * * python3 /Users/weileizeng/Documents/GitHub/python-player/email/check_ip.py
-# run every hour at :30 mins
+print("""
+This script collect ip information and email it.
+Author: Weilei Zeng
+Dated: May 14, 2024
 
+To run it automatically for given peorid, add it into cron jobs
+cmd to edit cron job is `crontab -e`
+sample cmd
+30 * * * * python3 /Users/weileizeng/Documents/GitHub/python-player/email/check_ip.py
+run every hour at :30 mins
+""")
 
 import json
 import os
 
 #get current info
+ipify_ip=os.popen('curl https://api.ipify.org/').read()
 
-ip=os.popen('curl https://api.ipify.org/').read()
-user = os.popen('echo $USER').read()
-hostname = os.popen('hostname').read()
-date = os.popen('date').read()
+#use shell to get info
+#shell_user = os.popen('echo $USER').read()
+shell_user = os.popen('whoami').read()
+shell_hostname = os.popen('hostname').read()
+shell_date = os.popen('date').read()
 
+#use socket to get info
 import socket
 ## getting the hostname by socket.gethostname() method
 socket_hostname = socket.gethostname()
@@ -28,34 +38,50 @@ socket_public_ip=s.getsockname()[0]
 #print(s.getsockname()[0])
 s.close()
 
+from datetime import datetime
+# datetime object containing current date and time
+now = datetime.now() 
+#print("now =", now)
+# YYmmdd-HHMMSS
+python_date = now.strftime("%Y%m%d-%H%M%S")
 
 
 
-info=dict(ip=ip,user=user,hostname=hostname,date=date,
-          socket_hostname=socket_hostname,socket_local_ip=socket_local_ip,socket_public_ip=socket_public_ip)
+data_new=dict(ipify_ip=ipify_ip,
+              shell_user=shell_user,shell_hostname=shell_hostname,shell_date=shell_date,
+              socket_hostname=socket_hostname,socket_local_ip=socket_local_ip,socket_public_ip=socket_public_ip,
+              python_date=python_date)
 
-print(info)
+print('data_new',data_new)
+#print('all info',info)
 
-s=json.dumps(info,indent=2)
+#s=json.dumps(info,indent=2)
 #print(s)
 
-filename_current='data-latest.json'
 
-data_new=info
+filename_current='data-latest.json'
 #read previous data
 with open(filename_current,'r') as f:
     data_old=json.load(f)
 
 #compare data
-for k in ['ip','user','hostname','socket_local_ip','socket_public_ip','socket_hostname','date']:
-    if data_old[k] == data_new[k]:
-        pass
-    else:        
+for k in ['ipify_ip','shell_user','shell_hostname','socket_local_ip','socket_public_ip','socket_hostname','python_date','date']:
+    changed=False
+    if k not in data_new:
+        changed=True
+    elif k not in data_old:
+        changed=True    
+    elif data_old[k] != data_new[k]:
+        changed=True
+    if changed:
         print(k,'data changes. do something')
         from send import send_text
         ifconfig = os.popen('ifconfig').read()
-        content=dict(log=k+' changes ',data_old=data_old, data_new=data_new)
-        s=json.dumps(content,indent=2)
+        #content=dict(data_old=data_old, data_new=data_new)
+        s = k + ' changed. \n'
+        s = s + 'data_new:\n'+json.dumps(data_new,indent=2) + '\n'
+        s = s + 'data_old:\n'+json.dumps(data_old,indent=2) + '\n'        
+        #s=json.dumps(content,indent=2)
         s = s + '\n' + ifconfig
         send_text(s,note=' '+socket_hostname+' '+socket_public_ip)
         
@@ -63,16 +89,7 @@ for k in ['ip','user','hostname','socket_local_ip','socket_public_ip','socket_ho
 
 #save data
 
-from datetime import datetime
-
-# datetime object containing current date and time
-now = datetime.now()
- 
-#print("now =", now)
-
-# dd/mm/YY H:M:S
-dt_string = now.strftime("%Y%m%d-%H%M%S")
-filename_log='log/'+dt_string+'.json'
+filename_log='log/'+python_date+'.json'
 with open(filename_log,'w') as f:
     json.dump(info,f,indent=2)
     print('write into file',filename_log)
